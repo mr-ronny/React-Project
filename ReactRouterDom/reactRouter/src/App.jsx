6f4ddb07ea9bdd4ce9abb5ae67033bdb258ad348
mr-ronny <trialjunctiondavid0@gmail.com>
2024-09-07 14:10:18 +0530

    React-Router-Dom-Project

---
+++ ReactRouterDom/reactRouter/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import Header from './components/Header/Header'
+import Footer from './components/footer/Footer'
+import Home from './components/Home/Home'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1 className='bg-green-600 p-4'>react router</h1>
+    
+    </>
+  )
+}
+
+export default App
